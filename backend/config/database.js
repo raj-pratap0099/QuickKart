8b9781f1d9557483333374eacaf907b72046910c
raj-pratap0099 <rajendrapratap0099@gmail.com>
2026-03-29 23:16:06 +0530

    Update MongoDB connection URI environment variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGO_URI)
 
         console.log("DB connection succesfull")
 
