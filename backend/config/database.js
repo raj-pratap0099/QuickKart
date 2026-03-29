e05f09850ccfe3d9b1e2ce8057edf3e384b3a2be
raj-pratap0099 <rajendrapratap0099@gmail.com>
2026-03-29 23:21:18 +0530

    Update MongoDB

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGODB_URL)
 
         console.log("DB connection succesfull")
 
